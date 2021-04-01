8b40df73897196c5d294ec1b5e41a1f49fc7cfb6
Siya <67198163+Mar-Issah@users.noreply.github.com>
2021-03-31 21:43:00 -0400

    Update app.js file
    add some comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-// The data-* attribute gives us the ability to embed custom data attributes on all HTML elements. The stored (custom) data can then be used in the page's JavaScript to create a more engaging user experience (without any Ajax calls or server-side database queries).
-
 /*=====================
  retrieve element
 ======================*/
@@ -14,7 +12,6 @@ const allClearBtn = document.querySelector("[data-all-clear]");
 /*=====================
   create a class/blueprint of a calculator. which will state all functionalities/methods
 ======================*/
-//prevOperand, currentOperand and operation are all the properties of the class
 
 class Calculator {
 	constructor(prevOperationText, currentOperationText) {
@@ -31,22 +28,20 @@ class Calculator {
 		this.operation = undefined;
 	}
 
-	//convert to string to be able to use String.slice method. start form 0 index to last but one. so the current operand keeps the slice from 0 to last but one any time u click del leaving the final one out
+	//convert to string to be able to use String.slice method.
 	delete() {
 		this.currentOperand = this.currentOperand.toString().slice(0, -1);
 	}
 
-	//convert to string jus in case its a number so as to append/concatenate to the end of already typed numbers.
 	//check to see if the numbers have a period. you only want the user to enter one period
 	appendNumber(num) {
 		if (num === "." && this.currentOperand.includes(".")) return;
 		this.currentOperand = this.currentOperand.toString() + num.toString();
 	}
 
-	//this.operation property of the class is = to operation parameter. after you have set the operation (sign) to the property, set the currentOperand property to the prevOperand property as well and clear the current operand. to display it go to update fxn
 
-	//if the user doesnt enter any current text and click on the sign return....(it doent do anything) and the prev text still displays
-	//also if the prev operand is not empty then compute. so regardless of if you click th = or any sign it computes
+	//if the user doesnt enter any current text and click on the = sign, return.
+	//also if the prev operand is not empty then compute. 
 	operationSign(operation) {
 		if (this.currentOperand === "") return;
 
@@ -87,14 +82,14 @@ class Calculator {
 
 		//the current operand is the result
 		this.currentOperand = result;
-		this.operation = undefined; //undefined means it doesnt have a value
+		this.operation = undefined;
 		this.prevOperand = "";
 	}
 
 	//we want the commas in our long numbers. use it in the update output below
 	//to locale string is an inbuilt formater
 	formatNumber(num) {
-		const stringNum = num.toString(); //just making sure its a string before spliting into array by "." and taking the array index 0 which is the integer before the decimal or "."
+		const stringNum = num.toString(); 
 
 		const integerNums = parseFloat(stringNum.split(".")[0]);
 		const decimalNums = stringNum.split(".")[1];
@@ -105,32 +100,28 @@ class Calculator {
 			integerDisplay = integerNums.toLocaleString("en", {
 				maximumFractionDigits: 0,
 			});
-			//makeing sure there are no decimal places after the integer display
+			
 		}
 
 		if (decimalNums != null) {
-			//this means the user enter decimals. in that case separate the integr.decimal else just return only the decimals
+			//this means the user enter decimals. in that case separate the integer.decimal else just return only the decimals
 			return `${integerDisplay}.${decimalNums}`;
 		} else {
 			return integerDisplay;
 		}
 
-		// const floatNum = parseFloat(num);
-		// if (isNaN(floatNum)) return "";
-		// return floatNum.toLocaleString("en");
 	}
 
-	//we also want to diplay the operation sign
 	updateOutput() {
 		this.currentOperationText.innerHTML = this.formatNumber(
 			this.currentOperand
 		);
 		if (this.operation != null) {
 			this.prevOperationText.innerHTML = `${this.prevOperand} ${this.operation}`;
-			//do your setting/updating here after receiving the text
+			
 		} else {
 			this.prevOperationText.innerHTML = "";
-			//if you dont type any ops but the = or any number, then clear the prev opeation
+			
 		}
 	}
 }
